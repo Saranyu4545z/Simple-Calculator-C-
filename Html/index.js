cc130c4dedf93549ec03062b794e57f05b8dc63f
Saranyu4545z <684244136@mail.pbru.ac.th>
2025-10-30 14:48:21 +0700

    Add files via upload

---
+++ Html/index.js
@@ -0,0 +1,20 @@
+// CALCULATOR PROGRAM
+
+const display = document.getElementById("display");
+
+function appendToDiplay(input){
+    display.value +=input;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function calculate(){
+    try{
+        display.valuen = eval(display.value);
+    }
+    catch(error){
+        display.value = "Error";
+    }
+}
